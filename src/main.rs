c27deebde8809827bd4894e1c9a573a1a5d79232
Tobias Clasen <sandalbanditten@tutanota.com>
2025-12-13 19:30:36 +0100

    Initial iced usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Element;
+use iced::widget::{button, column, text};
+
+pub fn main() -> iced::Result {
+    iced::run(update, view)
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+#[derive(Default)]
+struct Counter {
+    value: u64,
+}
+
+fn update(counter: &mut Counter, message: Message) {
+    match message {
+        Message::Increment => counter.value += 1,
+    }
+}
+
+fn view(counter: &Counter) -> Element<'_, Message> {
+    column![
+        text(counter.value).size(20),
+        button("Increment").on_press(Message::Increment),
+    ]
+    .spacing(10)
+    .into()
 }
